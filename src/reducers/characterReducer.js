a10d1dc925195dd5a95675b63bfe393620958a8a
Kamran Ali <kgali21@gmail.com>
2019-11-19 07:26:46 -0800

    combine reducer set up

--- src/reducers/characterReducer.js
+++ src/reducers/characterReducer.js
@@ -6,4 +6,11 @@ const initialState = {
   profession: ''
 };
 
-export default reducer(state = initialState, action)
+export default function reducer(state = initialState, action){
+  switch(action.type){
+    case SET_CHARACTER:
+      return action.payload;
+    default:
+      return state;
+  }
+}
