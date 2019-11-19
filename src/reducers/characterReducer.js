8ccab9ebe9b469cdeadc9c9dcb0b8e5cc650e859
Kamran Ali <kgali21@gmail.com>
2019-11-19 07:24:02 -0800

    actions and reducers set up

---
+++ src/reducers/characterReducer.js
@@ -0,0 +1,9 @@
+import { SET_CHARACTER } from '../actions/actions';
+
+const initialState = {
+  photoUrl: '',
+  name: '',
+  profession: ''
+};
+
+export default reducer(state = initialState, action)
